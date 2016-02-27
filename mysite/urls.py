dc54fef5e4530c46b9954ac5dd9c403b3e29d5ed
Maja108 <majamagierowska@gmail.com>
2016-02-27 15:57:34 +0100

    Zielony

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,17 @@
+#from django.conf.urls import url
+##from . import views
+#from django.shortcuts import render
+
+# Create your views here.
+#urlpatterns = [
+#    url(r'^$', views.post_list, name='post_list'),
+#]
+
+
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
